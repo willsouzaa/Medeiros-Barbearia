7f7ed2eb869ecd6b1873832d5b24f44b087a70b2
willsouzaa <pauloamancio79@gmail.com>
2024-09-30 21:55:09 -0300

    estilizaçao do google maps para telas menores

--- medeiros/src/App.js
+++ medeiros/src/App.js
@@ -41,9 +41,6 @@ function App() {
                     <iframe
                         title="Mapa da Barbearia"
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d229.61947220776176!2d-48.665370401517634!3d-27.62064536178305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95273542bc0551fd%3A0x7f013ddaa9edf83c!2sBarbershop%20Medeiros!5e0!3m2!1spt-BR!2sbr!4v1727743027149!5m2!1spt-BR!2sbr"
-                        width="600"
-                        height="450"
-                        style={{ border: 0 }}
                         allowFullScreen=""
                         loading="lazy"
                     ></iframe>
@@ -52,7 +49,6 @@ function App() {
                 <section id="contatos">
                     <Contatos />
                 </section>
-                
             </main>
         </div>
     );
