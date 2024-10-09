51d018d6ed2e1d4b266dc08791ad94cea39b568f
willsouzaa <pauloamancio79@gmail.com>
2024-10-08 21:59:46 -0300

    botao de mapa criado

--- medeiros/src/App.js
+++ medeiros/src/App.js
@@ -28,6 +28,7 @@ function App() { // Define o componente funcional App.
                 </section>
                 <section id="localizacao"> {/* Seção para localização */}
                     <h2>Localização</h2>
+                    
                     <iframe
                         title="Mapa da Barbearia" // Acessibilidade para o iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d229.61947220776176!2d-48.665370401517634!3d-27.62064536178305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95273542bc0551fd%3A0x7f013ddaa9edf83c!2sBarbershop%20Medeiros!5e0!3m2!1spt-BR!2sbr!4v1727743027149!5m2!1spt-BR!2sbr"
@@ -35,6 +36,17 @@ function App() { // Define o componente funcional App.
                         loading="lazy" // Carregamento atrasado para melhorar a performance.
                     ></iframe>
                 </section>
+                <div className='botaoMapa-container'>
+                    <a
+                    href={`https://maps.google.com/maps/dir//Barbershop+Medeiros+R.+Valdemar+Vi%C3%AAira,+1415+-+Jardim+Eldorado+Palho%C3%A7a+-+SC+88133-390/@-27.6205542,-48.6651694,20z/data=!4m5!4m4!1m0!1m2!1m1!1s0x95273542bc0551fd:0x7f013ddaa9edf83c`} // Link Site de agendamento online.
+                    target="_blank" // Abre o link em uma nova aba.
+                    rel="noopener noreferrer" // Melhora a segurança ao abrir um link em nova aba.
+                    className="irParaRotas-button" // Classe para estilização do botão.
+                >
+                    Abrir no Google Maps {/* Texto do botão */}
+                </a>
+                </div>
+              
                 <section id="contatos"> {/* Seção para o componente Contatos */}
                     <Contatos />
                 </section>
@@ -59,6 +71,7 @@ function App() { // Define o componente funcional App.
             </div>
             
         </div>
+        
     );
 }
 
